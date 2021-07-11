0c22fae1c8ccf29c989d229030a86edb207bb908
Miguel Angel Carvajal Gonzalez <amcarvajal15@gmail.com>
2021-07-11 23:12:07 +0000

    Traffci Light Done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "./styles.css";
 
 //create your first component
 export function Home() {
+	const [colorRed, setColorRed] = useState("");
+	const [colorGreen, setColorGreen] = useState("");
+	const [colorYellow, setColorYellow] = useState("");
+
+	const redLight = () => {
+		setColorRed("light");
+		setColorGreen("");
+		setColorYellow("");
+	};
+
+	const greenLight = () => {
+		setColorRed("");
+		setColorGreen("light");
+		setColorYellow("");
+	};
+	const yellowLight = () => {
+		setColorRed("");
+		setColorGreen("");
+		setColorYellow("light");
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<div
+					className={`lightRed  ${colorRed}`}
+					onClick={redLight}></div>
+				<div
+					className={`lightGreen  ${colorGreen}`}
+					onClick={greenLight}></div>
+				<div
+					className={`lightYellow  ${colorYellow}`}
+					onClick={yellowLight}></div>
+			</div>
 		</div>
 	);
 }
